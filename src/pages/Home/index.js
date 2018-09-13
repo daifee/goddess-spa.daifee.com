55c1c21d048482a88dec556d4d89787be5068e3b
daifee <mr_zhangjiayong@163.com>
2018-09-14 00:57:14 +0800

    首页还差内容布局

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
+import {NavBar, Tabs} from 'antd-mobile';
+import './styles.css';
+
+const TABS = [
+  {title: '美女'},
+  {title: '风景'},
+  {title: '萌宠'}
+];
 
 export default class Home extends React.Component {
 
   render() {
     return (
-      <main>
-        <h1>Home</h1>
-        <p>
-          <Link to='/login'>登录</Link>
-          <Link to='/register'>注册</Link>
-        </p>
+      <main id='home'>
+        <NavBar
+          mode='light'
+          leftContent='主页'
+          rightContent='编辑'
+        >
+          daifee.com
+        </NavBar>
+        <Tabs
+          tabs={TABS}
+          initialPage={0}
+        >
+          {this.renderContent}
+        </Tabs>
       </main>
     );
+  }
+
+  renderContent(tab) {
+    return (<div className='content'>{tab.title}</div>);
   }
 }
