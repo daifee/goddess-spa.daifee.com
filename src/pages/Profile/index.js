65a46279a3507c881a3f275e0134b9e5ed9f71b2
daifee <mr_zhangjiayong@163.com>
2018-09-21 01:30:50 +0800

    定义路由和UI

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -12,18 +12,61 @@
 
 import React from 'react';
 import {connect} from 'react-redux';
+import dayjs from 'dayjs';
 import {SubPage} from '../../components/Page';
 import {getState as getGlobalState} from '../../store';
+
+// import {} from 'antd-mobile';
 
 import './styles.css';
 
 
 class Profile extends React.Component {
+  goToRecommend = () => {
+    const {user, history} = this.props;
+    history.push(`/users/${user.id}/blogs/?recommend=all`);
+  };
+
+  goToLikes = () => {
+    const {user, history} = this.props;
+    history.push(`/users/${user.id}/likes/`);
+  };
+
+  goToSettings = () => {
+    const {history} = this.props;
+    history.push(`/settings`);
+  };
+
   render() {
     const {user} = this.props;
+
     return (
       <SubPage id='profile' navBar={{children: user.name}}>
-        <h1>Profile</h1>
+        <header>
+          <div className='user'>
+            <div className='avatar'>
+              <img
+                alt='头像'
+                src='https://tva2.sinaimg.cn/crop.0.0.180.180.180/852a97e7jw1e8qgp5bmzyj2050050aa8.jpg'
+              />
+            </div>
+            <div className='info'>
+              <h2>{user.name}</h2>
+              <p>{dayjs(user.createdAt).format('YYYY-MM-DD')}</p>
+            </div>
+          </div>
+          <div className='gird-box'>
+            <div onClick={this.goToRecommend}>
+              <span>被推荐</span>
+            </div>
+            <div onClick={this.goToLikes}>
+              <span>收藏</span>
+            </div>
+            <div onClick={this.goToSettings}>
+              <span>设置</span>
+            </div>
+          </div>
+        </header>
       </SubPage>
     );
   }
@@ -37,3 +80,4 @@ export default connect((rootState, props) => {
     ...props
   };
 })(Profile);
+
