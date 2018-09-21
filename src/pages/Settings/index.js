1f860766ec71e483af10cd7631b94f904c1ce071
daifee <mr_zhangjiayong@163.com>
2018-09-22 00:26:03 +0800

    定义管理员入口

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -9,9 +9,19 @@ import pkg from '../../../package.json';
 import './styles.css';
 
 class Settings extends React.Component {
-  handleAbout = () => {
+  pushAbout = () => {
     const {history} = this.props;
     history.push('/about');
+  };
+
+  pushAllUsers = () => {
+    const {history} = this.props;
+    history.push('/admin/users');
+  };
+
+  pushAllBlogs = () => {
+    const {history} = this.props;
+    history.push('/admin/blogs');
   };
 
   handleLogout = async () => {
@@ -22,6 +32,8 @@ class Settings extends React.Component {
   };
 
   render() {
+    const {user} = this.props;
+
     return (
       <SubPage id='settings' navBar={{children: '设置'}}>
 
@@ -30,12 +42,26 @@ class Settings extends React.Component {
         <List className='section-1'>
           <List.Item
             arrow='horizontal'
-            onClick={this.handleAbout}
+            onClick={this.pushAbout}
             extra='daifee.com'
           >
             关于
           </List.Item>
         </List>
+
+        {user.role === 'admin' ? (
+          <React.Fragment>
+            <WhiteSpace />
+            <List>
+              <List.Item arrow='horizontal' onClick={this.pushAllUsers}>
+                所有用户
+              </List.Item>
+              <List.Item arrow='horizontal' onClick={this.pushAllBlogs}>
+                所有微博
+              </List.Item>
+            </List>
+          </React.Fragment>
+        ) : null}
 
         <WhiteSpace />
 
