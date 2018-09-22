188fe3838099fbb1fdce79c076df75db6b90f06b
daifee <mr_zhangjiayong@163.com>
2018-09-22 11:23:00 +0800

    独立配置route

--- src/Router.js
+++ src/Router.js
@@ -14,11 +14,71 @@ import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import Settings from './pages/Settings';
 import UserBlogList from './pages/UserBlogList';
-import UserLikes from './pages/UserLikes';
 
+import UserLikes from './pages/UserLikes';
 import UserList from './pages/UserList';
 import BlogList from './pages/BlogList';
 import EditBlog from './pages/EditBlog';
+
+
+const config = [
+  {
+    path: '/',
+    component: Home,
+    _route: Route,
+  },
+  {
+    path: '/login',
+    component: Login,
+    _route: TouristRoute
+  },
+  {
+    path: '/register',
+    component: Register,
+    _route: TouristRoute
+  },
+  {
+    path: '/settings',
+    component: Settings,
+    _route: UserRoute
+  },
+  {
+    path: '/users/:id',
+    component: Profile,
+    _route: UserRoute
+  },
+  {
+    path: '/users/:id/blogs',
+    component: UserBlogList,
+    _route: UserRoute
+  },
+  {
+    path: '/users/:id/likes/',
+    component: UserLikes,
+    _route: UserRoute
+  },
+  {
+    path: '/blogs/edit',
+    component: EditBlog,
+    _route: UserRoute
+  },
+  {
+    path: '/admin/users',
+    component: UserList,
+    _route: UserRoute
+  },
+  {
+    path: '/admin/blogs',
+    component: BlogList,
+    _route: UserRoute
+  },
+  {
+    exact: false,
+    component: NotFound,
+    _route: Route
+  }
+];
+
 
 /**
  * 初始化登录后再注册路由
@@ -39,17 +99,14 @@ class Router extends React.Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' component={Home} />
-          <TouristRoute exact path='/login' component={Login}  user={user} />
-          <TouristRoute exact path='/register' component={Register} user={user} />
-          <UserRoute exact path='/settings' component={Settings} user={user} />
-          <UserRoute exact path='/users/:id' component={Profile} user={user} />
-          <UserRoute exact path='/users/:id/blogs/' component={UserBlogList} user={user} />
-          <UserRoute exact path='/users/:id/likes/' component={UserLikes} user={user} />
-          <UserRoute exact path='/blogs/edit' component={EditBlog} user={user} />
-          <UserRoute exact path='/admin/users/' component={UserList} user={user} />
-          <UserRoute exact path='/admin/blogs/' component={BlogList} user={user} />
-          <Route component={NotFound} />
+          {config.map(({_route: Route, ...rest}, index) => {
+            // 默认 `exact=true`
+            if (typeof rest.exact === 'undefined') {
+              rest.exact = true;
+            }
+            const key = `${index}-${rest.path}`;
+            return (<Route key={key} {...rest} user={user} />);
+          })}
         </Switch>
       </BrowserRouter>
     );
