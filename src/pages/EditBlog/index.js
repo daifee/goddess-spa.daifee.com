8788af958721536135e489d7aa4ae3937dbe6f11
daifee <mr_zhangjiayong@163.com>
2018-09-22 12:17:13 +0800

    完成UI

--- src/pages/EditBlog/index.js
+++ src/pages/EditBlog/index.js
@@ -1,14 +1,50 @@
 import React from 'react';
-import {SubPage} from '../../components/Page';
+import { List, TextareaItem, WingBlank, WhiteSpace, ImagePicker, NavBar } from 'antd-mobile';
 import './styles.css';
 
 class EditBlog extends React.Component {
+  handleChange = (files, type, index) => {
+    console.log(files, type, index);
+  };
 
   render() {
     return (
-      <SubPage id='edit-blog' navBar={{children: '发布微博'}}>
-        <h1>TODO</h1>
-      </SubPage>
+      <main id='edit-blog'>
+        <NavBar
+          mode='light'
+          leftContent='取消'
+          onLeftClick={this.handleCancel}
+          rightContent={(
+            <div
+              className='right-btn'
+              onClick={this.handlePublish}
+            >
+              <span>发布</span>
+            </div>
+          )}
+        >
+          发布微博
+        </NavBar>
+
+        <List>
+          <TextareaItem
+            placeholder='请输入...'
+            autoHeight
+            rows={2}
+            count={140}
+          />
+        </List>
+
+        <WhiteSpace />
+
+        <WingBlank>
+          <ImagePicker
+            files={[]}
+            onChange={this.handleChange}
+            selectable={true}
+          />
+        </WingBlank>
+      </main>
     );
   }
 }
